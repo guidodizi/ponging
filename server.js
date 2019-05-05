9166532998a19a7fc652f57c21d3bb4d3ae2a33f
Guido Dizioli <guidodizi@gmail.com>
2019-05-05 16:36:39 -0300

    modularization

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
